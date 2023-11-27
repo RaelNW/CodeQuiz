721d30a4b1203e3d82310b8b490ba9fb2623c674
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-27 00:59:36 -0800

    Added a questions to the javascript

--- index.js
+++ index.js
@@ -10,3 +10,57 @@ const timeLeft = document.getElementById("timeLeft");
 const timer = document.getElementById("timer");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const initialsInput = document.getElementById("initialsInput");
+
+// Set the initial state of the game
+let currentQuestionIndex = 0;
+let timerInterval;
+let score = 0;
+let time = 60; // Set the initial time in seconds
+
+const questions = [
+    {
+        question:
+        "What is the HTML tag under which one can write the JavaScript code?",
+        answers: ["<javascript>", "<scripted>", "<script>", "<js>"],
+        correctAnswer: "<script>",
+    },
+    {
+        question:
+        "What is the HTML tag under which one can write the JavaScript code?",
+        answers: ["<javascript>", "<scripted>", "<script>", "<js>"],
+        correctAnswer: "<script>",
+    },
+
+    {
+        question:
+        "What is the correct syntax for referring to an external script called “geek.js”?",
+        answers: [
+        "<script src=”geek.js”>",
+        "<script href=”geek.js”>",
+        "<script ref=”geek.js”>",
+        "<script name=”geek.js”>",
+        ],
+        correctAnswer: "<script src=”geek.js”>",
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: [
+        "<!--This is a comment-->",
+        "//This is a comment",
+        "–This is a comment",
+        "**This is a comment**",
+        ],
+        correctAnswer: "//This is a comment",
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        answers: [
+        "var colors = “red”, “green”, “blue”",
+        "var colors = 1 = (“red”), 2 = (“green”), 3 = (“blue”)",
+        "var colors = (1:”red”, 2:”green”, 3:”blue”)",
+        "var colors = [“red”, “green”, “blue”]",
+        ],
+        correctAnswer: "var colors = [“red”, “green”, “blue”]",
+    },
+];
+
