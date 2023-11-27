5e7528c8c737d8b3d19110c751bbdb95b28eaf4d
Rael Wanjala <raelnwanjala@gmail.com>
2023-11-27 00:52:44 -0800

    Added event listeners

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//Add event listeners
+const startBtn = document.getElementById("startBtn");
+const questionContainer = document.getElementById("questionContainer");
+const questionText = document.getElementById("questionText");
+const answersContainer = document.getElementById("answers");
+const resultContainer = document.getElementById("resultContainer");
+const resultText = document.getElementById("resultText");
+const timeLeft = document.getElementById("timeLeft");
+const timer = document.getElementById("timer");
+const saveScoreBtn = document.getElementById("saveScoreBtn");
+const initialsInput = document.getElementById("initialsInput");
